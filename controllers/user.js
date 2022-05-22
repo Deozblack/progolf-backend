2ec9f41fcde833c7d69aaf9eb2b5d58d99d699b0
Omar Jimenez <omar_solanoxnr@hotmail.com>
2022-05-22 15:58:51 -0500

    Creando los endpoints faltantes... eliminar, obtener y actualizar

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,10 @@ const User = require("../models/User");
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/jwt");
 
+
+
+// Funcion para crear un usuario
+
 const crearUsuario = async (req, res = response) => {
   const { nombre, apellido, correo, password } = req.body;
 
@@ -45,11 +49,14 @@ const crearUsuario = async (req, res = response) => {
     console.log(error);
     return res.status(500).json({
       ok: error,
-      msg: "Error en el server, contactar al admin",
+      msg: 'Error en la App!!'
     });
 
   }
 };
+
+
+// Funcion para hacer el login a la aplicacion
 
 const login = async (req,res = response) =>{
   const {correo, password} = req.body;
@@ -81,7 +88,7 @@ const login = async (req,res = response) =>{
 
     //respuesta del servicio
 
-    return res.json({
+    return res.status(201).json({
       ok: true,
       uid:dbUsuario.id,
       nombre: dbUsuario.nombre,
@@ -92,12 +99,14 @@ const login = async (req,res = response) =>{
     
   } catch (error) {
     console.log(error);
-    return res.json({
+    return res.status(500).json({
       ok: false,
-      msg: "Error en el server, contactar al admin"
+      msg: 'Error en la App!!'
     })
   }
 }
+
+// Funcion para revalidar el token durante la navegacion del usuario en la app
 
 const revalidarToken = async(req, res = response) =>{
   const { uid, nombre, apellido, correo} = req;
@@ -113,8 +122,130 @@ const revalidarToken = async(req, res = response) =>{
   })
 }
 
+// Funcion que muestra los usuarios 
+
+const mostrarUsuarios = async( req, res = response) =>{
+  const usuarios = await User.find();
+
+  return res.status(201).json({
+    ok: true,
+    usuarios
+  })
+}
+
+// Funcion que obtine un usuario para posterior hacer modificacion sobre el
+
+const obtenerUsuario = async( req, res = response ) =>{
+  const uid = req.params.id;
+
+  try {
+
+    const usuario = await User.findById(uid);
+    
+    return res.status(201).json({
+      ok: true,
+      usuario
+    })
+
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok: false,
+      msg: 'Error en la App!!'
+    })
+  }
+}
+
+// Funcion para actualizar el usuario
+
+const actualizarUsuario = async( req, res = response ) =>{
+  const uid = req.params.id;
+
+  try {
+
+    // Buscamos el usario en la db
+    const dbUsuario = await User.findById(uid);
+
+
+    // Comprobamos que el usuario exista en la DB
+    if (!dbUsuario) {
+      return res.status(404).json({
+        ok:false,
+        msg: 'No existe ningún usuario con este ID'
+      })
+    }
+
+    // Comenzamos actualizar al usuario
+    // NOTA: Podemos extraer algun campo especificao y hacer modificaciones con el
+
+    const {...campos} = res.body;
+
+    // Realizamos la actualizacion en la DB
+
+    const usuarioActualizado = await User.findByIdAndUpdate(uid, campos, {new: true});
+
+    return res.status(201).json({
+      ok: true,
+      usuario: usuarioActualizado
+    })
+
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok:false,
+      mg: 'Error en la App!!'
+    })
+  }
+}
+
+// Funcion para eliminar usuarios
+
+const eliminarUsuario = async(req, res = response) =>{
+  const uid = req.params.id;
+
+  try {
+
+    // Buscamos el usuario en la DB
+    const usuario = await User.findById(uid);
+
+    // Comprobamos que el usuario exista en la BD
+    if (!usuario) {
+      return res.status(404).json({
+        ok:false,
+        msg: 'Este usuario no existe en la DB'
+      })
+    }
+
+    // Eliminamos al usuario de la DB
+    await User.findByIdAndDelete(uid);
+
+    return res.status(201).json({
+      ok:true,
+      msg: 'Usuario eliminado..'
+    })
+
+
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok:true,
+      msg: 'Error en la App!!'
+    })
+  }
+}
+
 module.exports = {
     login,
     revalidarToken,
-    crearUsuario
+    crearUsuario,
+    mostrarUsuarios,
+    obtenerUsuario,
+    actualizarUsuario,
+    eliminarUsuario
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { crearUsuario, login, revalidarToken } = require("../controllers/user");
+const { crearUsuario, login, revalidarToken, mostrarUsuarios, actualizarUsuario, obtenerUsuario, eliminarUsuario } = require("../controllers/user");
 const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
@@ -20,6 +20,23 @@ router.post('/new', [
     check('password', 'La contraseña es obligatoria...').notEmpty().isLength({min: 4}),
     check('correo', 'El correo es obligatorio...').notEmpty().isEmail(),
 ], crearUsuario);
+
+router.post('/user/:id', [
+    check('nombre', 'El nombre es obligatorio...').notEmpty().isLength({min:4}),
+    check('apellido', 'El apellido es obligatorio...').notEmpty().isLength({min: 4}),
+    check('telefono', 'El teléfono es obligatorio...').notEmpty().isLength({min: 10}),
+    check('cp', 'El código postal es obligatorio...').notEmpty().isLength({min: 5}),
+    check('password', 'La contraseña es obligatoria...').notEmpty().isLength({min: 4}),
+    check('correo', 'El correo es obligatorio...').notEmpty().isEmail(),
+], actualizarUsuario);
+
+router.get('/view', mostrarUsuarios);
+
+router.get('/user/:id', obtenerUsuario);
+
+router.delete('/user/delete/:id', eliminarUsuario);
+
+
 
 
 
